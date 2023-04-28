807939a43bb2d54eea809c120517d7753689fad5
Nyi Nyi <nyinyi@Nyis-MacBook-Air.local>
2023-04-28 10:32:43 +0630

    register route false

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Auth::routes(['register' => false]);
 Auth::routes();
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/', function () {
